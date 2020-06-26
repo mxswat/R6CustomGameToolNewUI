454b5fbf488179c1685293efd6f229c140312030
Massimo Mx Cacciapaglia <massimocacciapaglia.13@gmail.com>
2020-06-27 00:53:46 +0200

    Fucking overflows

---
+++ electron-frontend/src/defaults/guns.ts
@@ -0,0 +1,609 @@
+const GUNS = [
+    {
+        "name": "A-Rs",
+        "children": [
+            {
+                "name": "AR33",
+                "index": "0",
+                "type": "A-Rs"
+            },
+            {
+                "name": "L85A2",
+                "index": "1",
+                "type": "A-Rs"
+            },
+            {
+                "name": "G36C",
+                "index": "2",
+                "type": "A-Rs"
+            },
+            {
+                "name": "R4-C",
+                "index": "3",
+                "type": "A-Rs"
+            },
+            {
+                "name": "556XI",
+                "index": "4",
+                "type": "A-Rs"
+            },
+            {
+                "name": "V308",
+                "index": "5",
+                "type": "A-Rs"
+            },
+            {
+                "name": "F2",
+                "index": "6",
+                "type": "A-Rs"
+            },
+            {
+                "name": "SPEAR .308",
+                "index": "7",
+                "type": "A-Rs"
+            },
+            {
+                "name": "AK-12",
+                "index": "8",
+                "type": "A-Rs"
+            },
+            {
+                "name": "416-C CARBINE",
+                "index": "9",
+                "type": "A-Rs"
+            },
+            {
+                "name": "AUG A2",
+                "index": "10",
+                "type": "A-Rs"
+            },
+            {
+                "name": "552 COMMANDO",
+                "index": "11",
+                "type": "A-Rs"
+            },
+            {
+                "name": "C8-SFW",
+                "index": "12",
+                "type": "A-Rs"
+            },
+            {
+                "name": "9mm C1",
+                "index": "13",
+                "type": "A-Rs"
+            },
+            {
+                "name": "MK17 CQB",
+                "index": "14",
+                "type": "A-Rs"
+            },
+            {
+                "name": "PARA-308",
+                "index": "15",
+                "type": "A-Rs"
+            },
+            {
+                "name": "TYPE-89",
+                "index": "16",
+                "type": "A-Rs"
+            },
+            {
+                "name": "C7E",
+                "index": "17",
+                "type": "A-Rs"
+            },
+            {
+                "name": "PDW9",
+                "index": "18",
+                "type": "A-Rs"
+            },
+            {
+                "name": "T-95 LSW",
+                "index": "19",
+                "type": "A-Rs"
+            },
+            {
+                "name": "M762",
+                "index": "20",
+                "type": "A-Rs"
+            },
+            {
+                "name": "M4",
+                "index": "21",
+                "type": "A-Rs"
+            },
+            {
+                "name": "AK-74M",
+                "index": "22",
+                "type": "A-Rs"
+            },
+            {
+                "name": "AUG A3",
+                "index": "23",
+                "type": "A-Rs"
+            },
+            {
+                "name": "ARX200",
+                "index": "24",
+                "type": "A-Rs"
+            },
+            {
+                "name": "F90",
+                "index": "25",
+                "type": "A-Rs"
+            },
+            {
+                "name": "COMMANDO 9",
+                "index": "26",
+                "type": "A-Rs"
+            }
+        ]
+    },
+    {
+        "name": "LMGs",
+        "children": [
+            {
+                "name": "6P41",
+                "index": "27",
+                "type": "LMGs"
+            },
+            {
+                "name": "G8A1",
+                "index": "28",
+                "type": "LMGs"
+            },
+            {
+                "name": "M249",
+                "index": "29",
+                "type": "LMGs"
+            },
+            {
+                "name": "LMG-E",
+                "index": "30",
+                "type": "LMGs"
+            },
+            {
+                "name": "ALDA 5.56",
+                "index": "31",
+                "type": "LMGs"
+            },
+            {
+                "name": "M249 SAW",
+                "index": "32",
+                "type": "LMGs"
+            }
+        ]
+    },
+    {
+        "name": "DMR",
+        "children": [
+            {
+                "name": "417",
+                "index": "33",
+                "type": "DMR"
+            },
+            {
+                "name": "OTs-03",
+                "index": "34",
+                "type": "DMR"
+            },
+            {
+                "name": "CAMRS",
+                "index": "35",
+                "type": "DMR"
+            },
+            {
+                "name": "SR-25",
+                "index": "36",
+                "type": "DMR"
+            },
+            {
+                "name": "MK 14 EBR",
+                "index": "37",
+                "type": "DMR"
+            },
+            {
+                "name": "AR-15.50",
+                "index": "38",
+                "type": "DMR"
+            },
+            {
+                "name": "CSRX 300",
+                "index": "39",
+                "type": "DMR"
+            }
+        ]
+    },
+    {
+        "name": "SMG",
+        "children": [
+            {
+                "name": "FMG9",
+                "index": "40",
+                "type": "SMG"
+            },
+            {
+                "name": "MP5K",
+                "index": "41",
+                "type": "SMG"
+            },
+            {
+                "name": "SMG-11",
+                "index": "42",
+                "type": "SMG"
+            },
+            {
+                "name": "UMP45",
+                "index": "43",
+                "type": "SMG"
+            },
+            {
+                "name": "MP5",
+                "index": "44",
+                "type": "SMG"
+            },
+            {
+                "name": "P90",
+                "index": "45",
+                "type": "SMG"
+            },
+            {
+                "name": "9x19VSN",
+                "index": "46",
+                "type": "SMG"
+            },
+            {
+                "name": "MP7",
+                "index": "47",
+                "type": "SMG"
+            },
+            {
+                "name": "MPX",
+                "index": "48",
+                "type": "SMG"
+            },
+            {
+                "name": "M12",
+                "index": "49",
+                "type": "SMG"
+            },
+            {
+                "name": "MP5SD",
+                "index": "50",
+                "type": "SMG"
+            },
+            {
+                "name": "Bearing 9",
+                "index": "51",
+                "type": "SMG"
+            },
+            {
+                "name": "VECTOR .45 ACP",
+                "index": "52",
+                "type": "SMG"
+            },
+            {
+                "name": "T-5 SMG",
+                "index": "53",
+                "type": "SMG"
+            },
+            {
+                "name": "SCORPION EVO 3 A1",
+                "index": "54",
+                "type": "SMG"
+            },
+            {
+                "name": "K1A",
+                "index": "55",
+                "type": "SMG"
+            },
+            {
+                "name": "C75 Auto",
+                "index": "56",
+                "type": "SMG"
+            },
+            {
+                "name": "SMG-12",
+                "index": "57",
+                "type": "SMG"
+            },
+            {
+                "name": "Mx4 Storm",
+                "index": "58",
+                "type": "SMG"
+            },
+            {
+                "name": "SPSMG9",
+                "index": "59",
+                "type": "SMG"
+            },
+            {
+                "name": "P10 RONI",
+                "index": "60",
+                "type": "SMG"
+            }
+        ]
+    },
+    {
+        "name": "Shotguns",
+        "children": [
+            {
+                "name": "M590A1",
+                "index": "61",
+                "type": "Shotguns"
+            },
+            {
+                "name": "M1014",
+                "index": "62",
+                "type": "Shotguns"
+            },
+            {
+                "name": "SG-CQB",
+                "index": "63",
+                "type": "Shotguns"
+            },
+            {
+                "name": "SASG-12",
+                "index": "64",
+                "type": "Shotguns"
+            },
+            {
+                "name": "M870",
+                "index": "65",
+                "type": "Shotguns"
+            },
+            {
+                "name": "SUPER 90",
+                "index": "66",
+                "type": "Shotguns"
+            },
+            {
+                "name": "SPAS-12",
+                "index": "67",
+                "type": "Shotguns"
+            },
+            {
+                "name": "SUPERNOVA",
+                "index": "68",
+                "type": "Shotguns"
+            },
+            {
+                "name": "ITA12L",
+                "index": "69",
+                "type": "Shotguns"
+            },
+            {
+                "name": "ITA12S",
+                "index": "70",
+                "type": "Shotguns"
+            },
+            {
+                "name": "SIX12",
+                "index": "71",
+                "type": "Shotguns"
+            },
+            {
+                "name": "SIX12 SD",
+                "index": "72",
+                "type": "Shotguns"
+            },
+            {
+                "name": "FO-12",
+                "index": "73",
+                "type": "Shotguns"
+            },
+            {
+                "name": "BOSG",
+                "index": "74",
+                "type": "Shotguns"
+            },
+            {
+                "name": "ACS12",
+                "index": "75",
+                "type": "Shotguns"
+            },
+            {
+                "name": "TCSG12",
+                "index": "76",
+                "type": "Shotguns"
+            },
+            {
+                "name": "SUPER SHORTY",
+                "index": "77",
+                "type": "Shotguns"
+            }
+        ]
+    },
+    {
+        "name": "Pistols",
+        "children": [
+            {
+                "name": "P226 MK 25",
+                "index": "78",
+                "type": "Pistols"
+            },
+            {
+                "name": "M45 MEUSOC",
+                "index": "79",
+                "type": "Pistols"
+            },
+            {
+                "name": "5.7 USG",
+                "index": "80",
+                "type": "Pistols"
+            },
+            {
+                "name": "P9",
+                "index": "81",
+                "type": "Pistols"
+            },
+            {
+                "name": "LFP586",
+                "index": "82",
+                "type": "Pistols"
+            },
+            {
+                "name": "GSH-18",
+                "index": "83",
+                "type": "Pistols"
+            },
+            {
+                "name": "PMM",
+                "index": "84",
+                "type": "Pistols"
+            },
+            {
+                "name": "P12",
+                "index": "85",
+                "type": "Pistols"
+            },
+            {
+                "name": "MK1 9mm",
+                "index": "86",
+                "type": "Pistols"
+            },
+            {
+                "name": "D-50",
+                "index": "87",
+                "type": "Pistols"
+            },
+            {
+                "name": "LUISON",
+                "index": "88",
+                "type": "Pistols"
+            },
+            {
+                "name": "PRB92",
+                "index": "89",
+                "type": "Pistols"
+            },
+            {
+                "name": "P229",
+                "index": "90",
+                "type": "Pistols"
+            },
+            {
+                "name": "USP40",
+                "index": "91",
+                "type": "Pistols"
+            },
+            {
+                "name": "Q-929",
+                "index": "92",
+                "type": "Pistols"
+            },
+            {
+                "name": "RG15",
+                "index": "93",
+                "type": "Pistols"
+            },
+            {
+                "name": "KERATOS .357",
+                "index": "94",
+                "type": "Pistols"
+            },
+            {
+                "name": "Bailiff 410",
+                "index": "95",
+                "type": "Pistols"
+            },
+            {
+                "name": "1911 TACOPS",
+                "index": "96",
+                "type": "Pistols"
+            },
+            {
+                "name": "P-10C",
+                "index": "97",
+                "type": "Pistols"
+            },
+            {
+                "name": ".44 Mag Semi-Auto",
+                "index": "98",
+                "type": "Pistols"
+            },
+            {
+                "name": "SDP 9mm",
+                "index": "99",
+                "type": "Pistols"
+            }
+        ]
+    },
+    {
+        "name": "Shield",
+        "children": [
+            {
+                "name": "Le Roc",
+                "index": "100",
+                "type": "Shield"
+            },
+            {
+                "name": "Ballistic Shield",
+                "index": "101",
+                "type": "Shield"
+            },
+            {
+                "name": "G52-Tactical Shield",
+                "index": "102",
+                "type": "Shield"
+            },
+            {
+                "name": "CCE SHIELD",
+                "index": "103",
+                "type": "Shield"
+            }
+        ]
+    },
+    {
+        "name": "MISC",
+        "children": [
+            {
+                "name": "Ash's Launcher",
+                "index": "104",
+                "type": "MISC"
+            },
+            {
+                "name": "Capitao's Crossbow",
+                "index": "105",
+                "type": "MISC"
+            },
+            {
+                "name": "Hibana's Launcher",
+                "index": "106",
+                "type": "MISC"
+            },
+            {
+                "name": "Zofia's Launcher",
+                "index": "107",
+                "type": "MISC"
+            },
+            {
+                "name": "Mavrick's Blowtorch",
+                "index": "108",
+                "type": "MISC"
+            },
+            {
+                "name": "Amaru's Garahook",
+                "index": "109",
+                "type": "MISC"
+            },
+            {
+                "name": "Doc's Stims",
+                "index": "110",
+                "type": "MISC"
+            },
+            {
+                "name": "Mozzie's Pest Launcher",
+                "index": "111",
+                "type": "MISC"
+            },
+            {
+                "name": "Hands",
+                "index": "114",
+                "type": "MISC"
+            }
+        ]
+    }
+];
+
+export default GUNS;
--- electron-frontend/src/views/Host.vue
+++ electron-frontend/src/views/Host.vue
@@ -3,7 +3,7 @@
     <div class="History">
       <router-link to="/" class="link-btt underline-from-left">Home</router-link>
     </div>
-    <div class="container">
+    <div class="tabs-container">
       <div class="players tabs">
         <div class="selector"></div>
         <span class="player" v-for="(player, index) in players" :key="player.username">
@@ -18,8 +18,29 @@
           <label class="playerlabel" :for="'player-' + index">{{player.username}}</label>
         </span>
       </div>
-      <div class="weapons">{{selectedPlayer}}</div>
-      <div class="gadgets"></div>
+    </div>
+    <div class="overflow-container">
+      <div class="container">
+        <div class="list">
+          <div class="list-container">
+            <span>Weapons</span>
+            <div class="list-inner">
+              <div
+                class="category"
+                v-for="gunCategory in gunslibrary"
+                v-bind:key="gunCategory.name"
+              >
+                {{gunCategory.name}}
+                <div
+                  class="nodes"
+                  v-for="gun in gunCategory.children"
+                  v-bind:key="gun.index"
+                >{{gun.name}}</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -29,11 +50,24 @@ import { Component, Vue } from "vue-property-decorator";
 import { startTool, PlayerUpdated$ } from "../services/ipcfront";
 import { Subscription } from "rxjs";
 import animationTabs from "../defaults/hosttabs";
+import GUNS from "../defaults/guns";
+
 @Component
 export default class Host extends Vue {
   selectedPlayer = 0;
   players: Array<any> = [];
   subscriptions: Array<Subscription> = [];
+  ù;
+  // Definition copied from the VS UI
+  // TODO DEFINE TYPE
+  gunslibrary: {
+    name: string;
+    children: {
+      name: string;
+      index: string;
+      type: string;
+    }[];
+  }[] = [];
   created() {
     for (let i = 0; i < 10; i++) {
       this.players.push({
@@ -41,6 +75,7 @@ export default class Host extends Vue {
       });
     }
     // startTool();
+    this.gunslibrary = GUNS;
     this.subscribeToSubjects();
   }
   mounted() {
@@ -66,17 +101,37 @@ export default class Host extends Vue {
 </script>
 
 <style lang="scss" scoped>
+.overflow-container {
+  overflow: hidden;
+  display: flex;
+  flex-grow: 1;
+  flex-shrink: 1;
+  flex-basis: auto;
+}
+
 .host-page {
   width: 100%;
+  background: #36393f;
+  max-height: 100vh;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
 }
 
 .History {
-  padding-left: 16px;
-  padding-top: 16px;
+  padding: 8px 16px;
+  background: #202225;
 }
 
 .playerradio {
   display: none;
+}
+
+.tabs-container {
+  padding: 8px;
+  background: #2f3136;
+  overflow: hidden;
+  min-height: 35px;
 }
 
 .players {
@@ -89,14 +144,26 @@ export default class Host extends Vue {
   margin-right: 8px;
   display: block;
   cursor: pointer;
+  font-weight: 500;
 }
 .player {
   max-width: 150px;
+  z-index: 2;
 }
 
 input[type="radio"]:checked + label {
   font-weight: 600;
   border-radius: 20px;
+}
+
+.container {
+  display: grid;
+  grid-template-columns: repeat(3, [col] 1fr);
+  grid-template-rows: repeat(1, [row] auto);
+  gap: 8px;
+  flex-grow: 1;
+  flex-shrink: 1;
+  flex-basis: auto;
 }
 
 .selector {
@@ -110,6 +177,27 @@ input[type="radio"]:checked + label {
   transition-duration: 0.6s;
   transition-timing-function: cubic-bezier(0.68, -0.55, 0.265, 1.55);
   background: linear-gradient(45deg, #05abe0 0%, #8200f4 100%);
-  z-index: -1;
+  z-index: 0;
+}
+
+// LIST COMMON CODE
+.list-inner {
+  overflow: auto;
+}
+
+.list {
+  display: flex;
+  flex-grow: 1;
+  flex-shrink: 1;
+  flex-basis: auto;
+  overflow: hidden;
+}
+
+.list-container {
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  flex-shrink: 1;
+  flex-basis: auto;
 }
 </style>
