289a84f4fa51705c53bb9ed1ec7ff42e66b2ba70
Massimo Mx Cacciapaglia <massimocacciapaglia.13@gmail.com>
2020-06-29 18:40:10 +0200

    Gadget json

---
+++ electron-frontend/src/defaults/gadgets.ts
@@ -0,0 +1,328 @@
+const GADGETS = [
+    {
+        "name": "[Weapon Dependant(Will Replace Primary)]",
+        "children": [
+            {
+                "index": "Montange",
+                "name": "118",
+                "type": null
+            },
+            {
+                "index": "Blackbeard",
+                "name": "148",
+                "type": null
+            },
+            {
+                "index": "Glaz",
+                "name": "1C8",
+                "type": null
+            },
+            {
+                "index": "Clash",
+                "name": "198",
+                "type": null
+            },
+            {
+                "index": "Buck",
+                "name": "1F8",
+                "type": null
+            },
+            {
+                "index": "Blitz",
+                "name": "248",
+                "type": null
+            },
+            {
+                "index": "Kali",
+                "name": "Node0",
+                "type": null
+            }
+        ]
+    },
+    {
+        "name": "[Traps]",
+        "children": [
+            {
+                "index": "",
+                "name": "D8",
+                "type": null
+            },
+            {
+                "index": "Frost",
+                "name": "158",
+                "type": null
+            },
+            {
+                "index": "Ela",
+                "name": "230",
+                "type": null
+            },
+            {
+                "index": "Lesion",
+                "name": "270",
+                "type": null
+            },
+            {
+                "index": "Goyo",
+                "name": "278",
+                "type": null
+            },
+            {
+                "index": "Kapkan",
+                "name": "Node1",
+                "type": null
+            }
+        ]
+    },
+    {
+        "name": "[Holdable]",
+        "children": [
+            {
+                "index": "",
+                "name": "E0",
+                "type": null
+            },
+            {
+                "index": "Zofia",
+                "name": "F0",
+                "type": null
+            },
+            {
+                "index": "Hibana",
+                "name": "100",
+                "type": null
+            },
+            {
+                "index": "Maverick",
+                "name": "108",
+                "type": null
+            },
+            {
+                "index": "Amaru",
+                "name": "168",
+                "type": null
+            },
+            {
+                "index": "Doc",
+                "name": "1A8",
+                "type": null
+            },
+            {
+                "index": "Capitao",
+                "name": "1C0",
+                "type": null
+            },
+            {
+                "index": "Ash",
+                "name": "290",
+                "type": null
+            },
+            {
+                "index": "Mozzie",
+                "name": "Node2",
+                "type": null
+            }
+        ]
+    },
+    {
+        "name": "[Information]",
+        "children": [
+            {
+                "index": "",
+                "name": "E8",
+                "type": null
+            },
+            {
+                "index": "Valkyrie",
+                "name": "170",
+                "type": null
+            },
+            {
+                "index": "Warden",
+                "name": "190",
+                "type": null
+            },
+            {
+                "index": "Mira",
+                "name": "1E0",
+                "type": null
+            },
+            {
+                "index": "Alibi",
+                "name": "1E8",
+                "type": null
+            },
+            {
+                "index": "Jackal",
+                "name": "210",
+                "type": null
+            },
+            {
+                "index": "Dokkaebi",
+                "name": "220",
+                "type": null
+            },
+            {
+                "index": "Pulse",
+                "name": "228",
+                "type": null
+            },
+            {
+                "index": "Lion",
+                "name": "288",
+                "type": null
+            },
+            {
+                "index": "Iana",
+                "name": "258",
+                "type": null
+            },
+            {
+                "index": "Maestro",
+                "name": "268",
+                "type": null
+            },
+            {
+                "index": "Echo",
+                "name": "Node3",
+                "type": null
+            }
+        ]
+    },
+    {
+        "name": "[Denials]",
+        "children": [
+            {
+                "index": "",
+                "name": "120",
+                "type": null
+            },
+            {
+                "index": "Tachanka",
+                "name": "140",
+                "type": null
+            },
+            {
+                "index": "Bandit",
+                "name": "150",
+                "type": null
+            },
+            {
+                "index": "Twitch",
+                "name": "1D8",
+                "type": null
+            },
+            {
+                "index": "Rook",
+                "name": "160",
+                "type": null
+            },
+            {
+                "index": "Jager",
+                "name": "188",
+                "type": null
+            },
+            {
+                "index": "Castle",
+                "name": "1A0",
+                "type": null
+            },
+            {
+                "index": "Finka",
+                "name": "298",
+                "type": null
+            },
+            {
+                "index": "Thatcher",
+                "name": "218",
+                "type": null
+            },
+            {
+                "index": "Wamai",
+                "name": "1F0",
+                "type": null
+            },
+            {
+                "index": "Mute",
+                "name": "238",
+                "type": null
+            },
+            {
+                "index": "Recruit Primary Gadget Attack",
+                "name": "240",
+                "type": null
+            },
+            {
+                "index": "Recruit Primary Gadget Defense",
+                "name": "128",
+                "type": null
+            },
+            {
+                "index": "Nokk",
+                "name": "208",
+                "type": null
+            },
+            {
+                "index": "Caveira",
+                "name": "110",
+                "type": null
+            },
+            {
+                "index": "Ying",
+                "name": "130",
+                "type": null
+            },
+            {
+                "index": "Smoke",
+                "name": "1B0",
+                "type": null
+            },
+            {
+                "index": "Gridlock",
+                "name": "280",
+                "type": null
+            },
+            {
+                "index": "Oryx",
+                "name": "138",
+                "type": null
+            },
+            {
+                "index": "Melusi",
+                "name": "Node5",
+                "type": null
+            }
+        ]
+    },
+    {
+        "name": "[Destruction]",
+        "children": [
+            {
+                "index": "",
+                "name": "180",
+                "type": null
+            },
+            {
+                "index": "Sledge",
+                "name": "200",
+                "type": null
+            },
+            {
+                "index": "Thermite",
+                "name": "250",
+                "type": null
+            },
+            {
+                "index": "Fuze",
+                "name": "1B8",
+                "type": null
+            },
+            {
+                "index": "Ace",
+                "name": "Node6",
+                "type": null
+            }
+        ]
+    }
+]
+export default GADGETS
