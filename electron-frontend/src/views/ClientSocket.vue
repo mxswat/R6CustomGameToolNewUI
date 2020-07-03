7e4a79e7ca8c08598c50e71d9e26872f2f9d6f96
Massimo Mx Cacciapaglia <massimocacciapaglia.13@gmail.com>
2020-07-03 13:18:23 +0200

    look ok

--- electron-frontend/src/views/ClientSocket.vue
+++ electron-frontend/src/views/ClientSocket.vue
@@ -1,25 +1,44 @@
 <template>
   <div>
     Hello client
-    <a @click="sendMessage()"> Click me</a>
+    <input type="text" name="ip" id="ip" v-model="ipAddress"/>
+    <br />
+    <a @click="connectToSocketIoServer()">Connect to socket.io server</a>
+    <br />
+    <a @click="sendMessage">sendMessage</a>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import { Component } from 'vue-property-decorator'
-import io from 'socket.io-client';
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+import io from "socket.io-client";
 @Component
 export default class ClientSocket extends Vue {
-    socket: any;
-    created() {
-        const ipAddress = 'localhost';
-        const port = '3000';
-        this.socket = io('http://' + ipAddress + ':' + port);
-    }
+  socket: any;
+  ipAddress: string = 'ipgoeshere';
+  created() {
+    // const ipAddress = 'localhost';
+    // // TODO Reming me to use portfinder!
+    // const port = '3000';
+    // this.socket = io('http://' + ipAddress + ':' + port);
+  }
 
-    sendMessage() {
-        this.socket.emit('chat message', 'imAlive');
-    }
+  connectToSocketIoServer() {
+    const ipAddress = "localhost";
+    // TODO Reming me to use portfinder!
+    const port = "3000";
+    this.socket = io("http://" + this.ipAddress);
+  }
+
+  sendMessage() {
+    this.socket.emit("chat message", "imAlive");
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+a {
+  cursor: pointer;
+}
+</style>
